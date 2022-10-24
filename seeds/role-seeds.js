056063bc8372d45c88744e595a22faa4aeeb6ebe
T <endlesstruffle80@gmail.com>
2022-10-23 21:59:07 -0700

    adds seeding data

--- seeds/role-seeds.js
+++ seeds/role-seeds.js
@@ -1,6 +1,38 @@
 const { Role } = require('../models');
 
 const roleData = [
+  {
+    title: 'Sales Lead',
+    salary: 100000,
+  },
+  {
+    title: 'Sales Person',
+    salary: 80000,
+  },
+  {
+    title: 'Lead Engineer',
+    salary: 150000,
+  },
+  {
+    title: 'Software Engineer',
+    salary: 120000,
+  },
+  {
+    title: 'Account Manager',
+    salary: 160000,
+  },
+  {
+    title: 'Accountant',
+    salary: 125000,
+  },
+  {
+    title: 'Legal Team Lead',
+    salary: 250000,
+  },
+  {
+    title: 'Lawyer',
+    salary: 190000,
+  },
 ];
 
 const seedRoles = () => Role.bulkCreate(roleData);
